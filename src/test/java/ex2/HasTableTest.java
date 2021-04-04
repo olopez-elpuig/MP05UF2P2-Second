2b7ec4ca056b3dc234cc51372a16308c28e4e65f
olopez <olopez@elpuig.xeill.net>
2021-04-04 22:39:04 +0200

    add in

--- src/test/java/ex2/HasTableTest.java
+++ src/test/java/ex2/HasTableTest.java
@@ -8,20 +8,13 @@ public class HasTableTest {
     void count_size() {
         HashTable hashTable = new HashTable();
 
-        //comprueba que no hay ningun dato dentro del HashTable
         Assertions.assertEquals(0,hashTable.count());
-        //Comprueba que hay 16 posiciones en el hashtable
         Assertions.assertEquals(16,hashTable.size());
-        //        hashTable.put("test10","mundo4");
-        hashTable.put("1","mundo1");
-        hashTable.put("2","mundo2");
-        hashTable.put("3","mundo3");
+        hashTable.put("1","key1");
+        hashTable.put("2","key2");
+        hashTable.put("3","key3");
         Assertions.assertEquals(3,hashTable.count());
         Assertions.assertEquals(16,hashTable.size());
-
-        //hashTable.put("test10","mundo4");
-        //un error que no se solucionar
-        // la key es superior a 6 digitos
 
     }
 
@@ -30,39 +23,39 @@ public class HasTableTest {
         HashTable hashTable = new HashTable();
 
         Assertions.assertEquals("",hashTable.toString());
-        hashTable.put("1","mundo1");
-        hashTable.put("2","mundo2");
-        hashTable.put("3","mundo3");
-        hashTable.put("4","mundo4");
+        hashTable.put("1","key1");
+        hashTable.put("2","key2");
+        hashTable.put("3","key3");
+        hashTable.put("4","key4");
 
 
         Assertions.assertEquals("\n" +
-                " bucket[1] = [1, mundo1]\n" +
-                " bucket[2] = [2, mundo2]\n" +
-                " bucket[3] = [3, mundo3]\n" +
-                " bucket[4] = [4, mundo4]",hashTable.toString());
+                " bucket[1] = [1, key1]\n" +
+                " bucket[2] = [2, key2]\n" +
+                " bucket[3] = [3, key3]\n" +
+                " bucket[4] = [4, key4]",hashTable.toString());
 
     }
 
     @org.junit.jupiter.api.Test
     void getSinColision() {
         HashTable hashTable = new HashTable();
-        hashTable.put("1","mundo1");
-        hashTable.put("2","mundo2");
-        hashTable.put("3","mundo3");
-        hashTable.put("4","mundo4");
+        hashTable.put("1","key1");
+        hashTable.put("2","key2");
+        hashTable.put("3","key3");
+        hashTable.put("4","key4");
 
-        hashTable.put("2","otra_cosa");
-        hashTable.put("3","otra_cosa");
+        hashTable.put("2","difKey");
+        hashTable.put("3","difKey");
 
         Assertions.assertEquals("mundo1",hashTable.get("1"));
-        Assertions.assertEquals("otra_cosa",hashTable.get("3"));
+        Assertions.assertEquals("difKey",hashTable.get("3"));
     }
 
     @org.junit.jupiter.api.Test
     void getNoExiste() {
         HashTable hashTable = new HashTable();
-        hashTable.put("1","mundo1");
+        hashTable.put("1","key1");
         hashTable.get("12");
         Assertions.assertEquals(null,hashTable.get("12"));
     }
